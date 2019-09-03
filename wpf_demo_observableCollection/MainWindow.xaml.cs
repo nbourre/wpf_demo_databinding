ee672f1b181a480b7596a6f101c94f309ee1be99
nbourre <nbourre@hotmail.com>
2019-09-03 16:38:30 -0400

    Changement de nom d'objet et ajout d'un projet

---
+++ wpf_demo_observableCollection/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace wpf_demo_observableCollection
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            users.Add(new User() { Name = "John Doe" });
+            users.Add(new User() { Name = "Jane Doe" });
+
+            lbUsers.ItemsSource = users;
+
+            User user = null;
+
+            btnAddUser.Content = user?.Name ?? new User().Name;
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User() { Name = "Nouvel utilisateur" });
+        }
+
+        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                (lbUsers.SelectedItem as User).Name = "Nom aléatoire";
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                users.Remove(lbUsers.SelectedItem as User);
+        }
+    }
+
+    public class User : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+
+        }
+    }
+
+}
